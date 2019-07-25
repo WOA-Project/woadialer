238d1c2914ba809316cc3f4c5643b92059e161c5
Simone Franco <ratchet97@live.it>
2019-07-25 14:55:27 +0200

    Fixed Tapped Delay unwanted behavior

--- WoADialer/UI/Controls/DialPad.xaml.cs
+++ WoADialer/UI/Controls/DialPad.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Input;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,8 +13,6 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
-// Документацию по шаблону элемента "Пользовательский элемент управления" см. по адресу https://go.microsoft.com/fwlink/?LinkId=234236
 
 namespace WoADialer.UI.Controls
 {
@@ -24,6 +23,12 @@ namespace WoADialer.UI.Controls
         public DialPad()
         {
             this.InitializeComponent();
+            DigitButton[] digitbuttons = { NumPad_0, NumPad_1, NumPad_2, NumPad_3, NumPad_4, NumPad_5, NumPad_6, NumPad_7, NumPad_8, NumPad_9, NumPad_A, NumPad_S };
+            foreach (DigitButton digitbutton in digitbuttons)
+            {
+                digitbutton.AddHandler(PointerPressedEvent, new PointerEventHandler(DigitButton_PointerPressed), true);
+            }
+            NumPad_0.AddHandler(RightTappedEvent, new RightTappedEventHandler(DigitButton_RightTapped), true);
         }
 
         private void DigitButton_RightTapped(object sender, RightTappedRoutedEventArgs e)
@@ -33,18 +38,23 @@ namespace WoADialer.UI.Controls
                 switch(button.Digit)
                 {
                     case "0":
-                        DigitTapped?.Invoke(this, '+');
+                        if(e.PointerDeviceType == Windows.Devices.Input.PointerDeviceType.Touch)
+                        {
+                            DigitTapped?.Invoke(this, '+');
+                        }
                         break;
                 }
             }
         }
 
-        private void DigitButton_Tapped(object sender, TappedRoutedEventArgs e)
+        private void DigitButton_PointerPressed(object sender, PointerRoutedEventArgs e)
         {
             if (sender is DigitButton button)
             {
-                DigitTapped?.Invoke(this, button.Digit[0]);
-            }
+                PointerPoint pp = e.GetCurrentPoint(button);
+                if (pp.Properties.IsRightButtonPressed == true && button.Digit == "0") DigitTapped?.Invoke(this, '+');
+                else DigitTapped?.Invoke(this, button.Digit[0]);
+            } 
         }
     }
 }
